8d030b44b1e1c8ec060fc07233f79ccdd3343dc6
Abyhajon <joedabongo01@gmail.com>
2023-05-06 14:18:19 +0000

    database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Script creates a datbase anew user and grants the user privileges
+mysql -u root -p
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 
+	IF NOT EXISTS 'hbnb_dev'@'localhost' 
+	IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES 
+	ON hbnd_dev_db.* 
+	TO 'hbnb_dev'@'localhost';
+GRANT SELECT 
+	ON performance_schema.* 
+	TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
